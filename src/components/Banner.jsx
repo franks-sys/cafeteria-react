cc9207f1ef331add2f52c4d49f5a72c8681c9130
franks-sys <frankss>
2026-08-14 17:03:55 -0300

    vercel

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+function Banner() {
+    return(
+        <section className="hero" id="inicio">
+            <div className="container hero-conteudo">
+                <p className="destaque">Café Especial,
+                    todos os dias0</p>
+                    <h1>Uma pausa saborosa para o seu dia</h1>
+
+                    <p>Conheca nossos cafpes, bebidas e doces</p>
+                    <a className="botao" href="#cardapio">Ver cardapio</a>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Sobre.jsx
@@ -0,0 +1,15 @@
+function Sobre(){
+    return (
+        <section className="secao" id="sobre">
+            <div className="container sobre">
+                <div>
+                    <a className="destaque">Nossa História</a>
+                    <h2>MAis que cafe, um lugar para se econtrar</h2>
+                    <p>Cafeteria Aroma nasceu para oferecer um ambiente acolhedor , com cafés salecionados e atendimento proximo</p>
+                    <p>Nosso objetivo é transformar café em historia</p>
+                </div>
+                <div className="ilustracao">☕</div>
+            </div>
+        </section>
+    )
+}
